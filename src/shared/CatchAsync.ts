fea055091ae0315433dd06155cb508cf04c6bf09
younusFoysal <younus.faysal@gmail.com>
2025-04-18 06:23:39 -0400

    response and async function global added

---
+++ src/shared/CatchAsync.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, RequestHandler, Response } from "express"
+
+const CatchAsync=(fn:RequestHandler)=>{
+    return async(req:Request,res:Response,next:NextFunction)=>{
+        try{
+            await fn(req,res,next)
+        }catch(err){
+            next(err)}
+    }
+}
+
+export default CatchAsync;
---
+++ src/shared/SendResponse.ts
@@ -0,0 +1,22 @@
+import { Response } from "express";
+
+const SendResponse=<T>(res:Response,jsondata:{
+    statusCode: number,
+    success: boolean,
+    message: string,
+    meta?: {
+        page: number,
+        limit: number,
+        total: number
+    },
+    data: T | null | undefined
+} )=>{
+    res.status(jsondata.statusCode).json({
+        success:jsondata.success,
+        message:jsondata.message,
+        meta:jsondata.meta || null || undefined,
+        data:jsondata.data || null || undefined
+    })
+}
+
+export default SendResponse;
